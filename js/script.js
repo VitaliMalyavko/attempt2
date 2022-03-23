af5711d362bb4257eca42e4d4d0f64767975a79e
VitaliMalyavko <95131243+VitaliMalyavko@users.noreply.github.com>
2022-03-24 01:12:49 +0300

    add swiper-slide and brand container

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper (".swiper", {
+
+  spaceBetween: 0,
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dinamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },  
+    
+    });
